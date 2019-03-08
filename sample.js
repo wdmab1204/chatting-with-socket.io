7ea82e9755a5928cfbf9abb5a3b81363b21d481d
SPKoon <wdmab1204@naver.com>
2019-03-08 15:12:32 +0900

    namespace test code

--- sample.js
+++ sample.js
@@ -1,64 +1,26 @@
-var app = require('express')();
-var server = require('http').createServer(app);
-// http server를 socket.io server로 upgrade한다
-var io = require('socket.io')(server);
+const app = require(‘express’)();
+const server = require(‘http’).Server(app);
+const io = require(‘socket.io’)(server);
 
-// localhost:3000으로 서버에 접속하면 클라이언트로 index.html을 전송한다
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+server.listen(3000, () => {
+  console.log(‘connect 3000’);
 });
 
-// connection event handler
-// connection이 수립되면 event handler function의 인자로 socket인 들어온다
-io.on('connection', function(socket) {
-
-  // 접속한 클라이언트의 정보가 수신되면
-  socket.on('login', function(data) {
-    console.log('%s(%s) is logined',data.name,data.userid);
-
-    // socket에 클라이언트 정보를 저장한다
-    socket.name = data.name;
-    socket.userid = data.userid;
-
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    io.emit('login', data.name );
-  });
-
-  // 클라이언트로부터의 메시지가 수신되면
-  socket.on('chat', function(data) {
-    console.log('%s : %s', socket.name, data.msg);
-
-    var msg = {
-      from: {
-        name: socket.name,
-        userid: socket.userid
-      },
-      msg: data.msg
-    };
-
-    // 메시지를 전송한 클라이언트를 제외한 모든 클라이언트에게 메시지를 전송한다
-    //socket.broadcast.emit('chat', msg);
-
-    // 메시지를 전송한 클라이언트에게만 메시지를 전송한다
-    //socket.emit('chat', msg);
-
-    // 접속된 모든 클라이언트에게 메시지를 전송한다
-    io.emit('chat', msg);
-
-    // 특정 클라이언트에게만 메시지를 전송한다
-    // io.to(id).emit('s2c chat', data);
-  });
-
-  // force client disconnect from server
-  socket.on('forceDisconnect', function() {
-    socket.disconnect();
-  })
-
-  socket.on('disconnect', function() {
-    console.log('user disconnected: ' + socket.name);
-  });
+app.get(‘/’, (req, res) => {
+  res.sendFile(__dirname + ‘/index.html’);
 });
 
-server.listen(3000, function() {
-  console.log('Socket IO server listening on port 3000');
+// NameSpace 1번
+const namespace1 = io.of(‘/namespace1’);
+
+// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
+namespace1.on(‘connection’, (socket) => {
+  namespace1.emit(‘news’, { hello: “Someone connected at namespace1” });
+});
+
+// NameSpace 2번
+const namespace2 = io.of(‘/namespace2’);
+// connection을 받으면, news 이벤트에 hello 객체를 담아 보낸다
+namespace2.on(‘connection’, (socket) => {
+  namespace2.emit(‘news’, { hello: “Someone connected at Namespace2” });
 });
